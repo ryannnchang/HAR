ae3dff29007cc522174a73d5f7425d8b94891787
ryannnchang <ryanc8764@gmail.com>
2025-07-03 19:16:18 +0100

    Updated mainV2.py with naming file methods

--- mainV2.py
+++ mainV2.py
@@ -8,17 +8,24 @@ from googlesheets import clear_values, update_values
 import datetime
 import json
 
+##Function to turn datetime into a JSON file
 def serialize_datetime(obj):
 	if isinstance(obj, datetime.datetime):
 		return obj.isoformat()
 		
+## Data Labelling
 data_labels = {0.0:"Walking", 1.0: "Walking Up", 2.0: "Walking Down", 3.0: "Sitting", 4.0: "Standing", 5.0: "Laying Down"}
 data_overall = []
+
+#Naming the Google Sheet
+run_name = str(input('Enter a run name: '))
+update_values('C1', 'RAW', [[run_name]])
 
 ##Clear Google sheets values
 clear_values()
 count = 0
 
+##Collecting data
 while True:
 	window = [[], [], []]
 	window_size = 128	
@@ -47,11 +54,7 @@ while True:
 	print(f"Prediction: {data_labels[prediction]} \t Confidence: {round(con*100)}%, \t Time: {date}")
 	
 	#Writing to Google Sheet 
-	update_values(f'A{count+3}:D{count+3}', 'RAW', [[json_date, data_labels[prediction], float(round(con*100)), data_collection_time]])
+	update_values(f'A{count+3}:D{count+3}', 'RAW', [[json_date, data_labels[prediction], float(con), data_collection_time]])
 	count += 1
-		
-		
-		
-	
 
 	
